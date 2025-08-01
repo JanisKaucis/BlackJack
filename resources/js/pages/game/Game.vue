59d8796207aa13e838b10456ba48c5edb62e19ec
JanisKaucis <janiskaucis@gmail.com>
2025-08-01 04:36:13 +0300

    ADD: game functionality with win/lose game

--- resources/js/pages/game/Game.vue
+++ resources/js/pages/game/Game.vue
@@ -4,8 +4,13 @@ import { onMounted, ref } from 'vue';
 
 const allCards = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const playerCards = ref<string[]>([]);
+const dealerCards = ref<string[]>([]);
 const playerCardsValue = ref(0);
-let lose = false;
+const dealerCardsValue = ref(0);
+const playerLose = ref(false);
+const dealerLose = ref(false);
+const playAgain = ref(false);
+const stay = ref(false);
 
 const cardValues = ref<Record<string, array>>({
     1: [1],
@@ -34,12 +39,71 @@ function drawTwoFirstCards() {
     console.log(playerCardsValue.value);
 }
 
-function drawACard() {
+async function playerDrawACard() {
     playerCards.value.push(getRandomCard());
     playerCardsValue.value = countCardValue(playerCards);
     console.log(playerCardsValue.value);
 
-    lose = checkIfLose(playerCardsValue)
+    playerLose.value = checkIfLose(playerCardsValue);
+
+    if (playerLose.value) {
+        await sleep(1000);
+        alert('You Lost')
+
+        playAgain.value = true;
+    }
+}
+
+async function drawDealerCards() {
+    const firstCard = getRandomCard();
+    const secondCard = getRandomCard();
+
+    dealerCards.value = [firstCard, secondCard];
+
+    dealerCardsValue.value = countCardValue(dealerCards);
+    console.log(dealerCardsValue.value);
+
+    while (dealerCardsValue.value <= 16 && dealerCardsValue.value < playerCardsValue.value) {
+        await sleep(1000);
+        await dealerDrawACard();
+    }
+
+    if (dealerLose.value) {
+        await sleep(1000);
+        alert('You Win');
+    }
+
+    if (dealerCardsValue.value <= 21 && dealerCardsValue.value > playerCardsValue.value) {
+        await sleep(1000);
+        alert('You Lost');
+    }else if (dealerCardsValue.value === playerCardsValue.value) {
+        await sleep(1000);
+        alert("It's a draw");
+    }
+
+    playAgain.value = true;
+}
+
+function startNewGame() {
+    drawTwoFirstCards();
+    dealerCards.value = [];
+    dealerCardsValue.value = 0;
+    playerLose.value = false;
+    dealerLose.value = false;
+    playAgain.value = false;
+    stay.value = false;
+}
+async function dealerDrawACard() {
+    dealerCards.value.push(getRandomCard());
+    dealerCardsValue.value = countCardValue(dealerCards);
+
+    dealerLose.value = checkIfLose(dealerCardsValue);
+}
+
+function stayWithHand() {
+    stay.value = true;
+
+    drawDealerCards();
 }
 
 function getRandomCard() {
@@ -65,8 +129,12 @@ function countCardValue(playerCards: object) {
     return cardsValue;
 }
 
-function checkIfLose(playerCardsValue :object) {
-    return playerCardsValue.value > 21;
+function checkIfLose(cardsValue: object) {
+    return cardsValue.value > 21;
+}
+
+function sleep(ms :number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 onMounted(() => {
@@ -76,18 +144,29 @@ onMounted(() => {
 
 <template>
     <div class="flex h-screen flex-col">
-        <div class="flex h-1/2 items-center justify-center">dealer cards</div>
+        <div class="flex h-1/2 items-center justify-center">
+            <div class="flex">
+                <div v-for="(card, index) in dealerCards" :key="index">
+                    <div class="rounded-sm border bg-white p-4 text-black">{{ card }}</div>
+                </div>
+            </div>
+        </div>
         <div class="flex h-1/2 items-center justify-center">
             <div class="flex">
                 <div v-for="(card, index) in playerCards" :key="index">
                     <div class="rounded-sm border bg-white p-4 text-black">{{ card }}</div>
                 </div>
             </div>
-            <div class="m-2" v-if="!lose">
-                <Button v-on:click="drawACard">Draw a Card</Button>
-            </div>
-            <div v-if="lose">
-                <p>You lost!</p>
+            <div class="flex justify-between m-2">
+                <div class="m-2">
+                    <Button :disabled="stay || playerLose" @click="playerDrawACard">Draw</Button>
+                </div>
+                <div class="m-2">
+                    <Button :disabled="stay || playerLose" @click="stayWithHand">Stay</Button>
+                </div>
+                <div v-if="playAgain" class="m-2">
+                    <Button @click="startNewGame">Play again</Button>
+                </div>
             </div>
         </div>
     </div>
